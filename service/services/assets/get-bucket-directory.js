3459e60dbd8d77368feec72c94c2305fa179c2a3
Fidel Romano <fidel.romano.2016@hotmail.com>
2024-02-21 01:57:11 -0300

    get bucket directory

--- service/services/assets/get-bucket-directory.js
+++ service/services/assets/get-bucket-directory.js
@@ -1,5 +1,5 @@
-const s3 = require('../../awsConfig.js')
-const { ClientError } = require('../../utils/err/errors')
+const s3 = require("../../awsConfig.js");
+const { ClientError } = require("../../utils/err/errors");
 
 const getDirectoryContent = async (bucketName, Prefix) => {
   // console.log('getDirectoryContent', { bucketName, Prefix })
@@ -7,21 +7,29 @@ const getDirectoryContent = async (bucketName, Prefix) => {
   const params = {
     Bucket: bucketName,
     Prefix,
-    OptionalObjectAttributes: ['Contents', 'IsTruncated']
-  }
+    OptionalObjectAttributes: ["Contents", "IsTruncated"],
+    // TagFilter: [
+    //   {
+    //     Key: "Tag1Key",
+    //     Value: "Tag1Value",
+    //   },
+    //   {
+    //     Key: "Tag2Key",
+    //     Value: "Tag2Value",
+    //   },
+    // ],
+  };
 
   return new Promise((resolve, reject) => {
     s3.listObjectsV2(params, (err, data) => {
       if (err) {
-        reject(new ClientError('Error getting directory contents', 500))
+        console.log({ err });
+        reject(new ClientError("Error getting directory contents", err));
       } else {
-        const contents = data.Contents.map((item) => item.Key)
-        // console.log('getDirectoryContent contents', contents)
-        // console.log('getDirectoryContent data.Contents', data.Contents)
-        resolve(data.Contents)
+        resolve(data.Contents);
       }
-    })
-  })
-}
+    });
+  });
+};
 
-module.exports = { getDirectoryContent }
+module.exports = { getDirectoryContent };
