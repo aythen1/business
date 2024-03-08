1121f3648cc28c0f3273f1a745787c20e8e7a2ea
aythen3 <info@aythen.com>
2024-03-08 18:28:57 +0100

    build: build

--- src/utils/apiBackend.js
+++ src/utils/apiBackend.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-// const axiosInstance = axios.create({
+const axiosInstance = axios.create({
   baseURL: 'https://business.ay-cloud.com/service/v1',
   // baseURL: "http://localhost:3001/service/v1",
   maxContentLength: Infinity,
